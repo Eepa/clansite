9d705960345b8fbcc6eeddea85002b3668f058cf
Eveliina <pevetiainen.ep@hotmail.com>
2014-03-13 16:11:44 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clansite::Application.config.secret_key_base = 'a462bc462b43b7787875f12c039848fbd8447a680725d62f3adebcd1f50ebd143be008557f99d876e8f17e2b11a04f2c7881dde8dfcd9aeeeafa8a2f71b87daa'
