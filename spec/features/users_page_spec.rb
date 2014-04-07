09509e9d9f660e0f01babc2fdbebfd754d95d895
Eveliina <pevetiainen.ep@hotmail.com>
2014-04-07 21:45:40 +0300

    VKO5 Lisatty user testit loppuun

--- spec/features/users_page_spec.rb
+++ spec/features/users_page_spec.rb
@@ -12,6 +12,25 @@ describe "Users page" do
     expect(page).to have_content 'Number of users: 0'
   end
 
+  it "should be able to create new user when given right username and password" do
+
+    visit signup_path
+
+    fill_in('user_name', with:"TestiU")
+    fill_in('user_password', with:"Test1")
+    fill_in('user_password_confirmation', with:"Test1")
+    click_button('Create User')
+
+
+
+    expect(page).to have_content 'TestiU'
+    expect(page).to have_content 'User was successfully created.'
+    expect(page).to have_content 'Clan: Not a member of any clan!'
+    expect(page).to have_content 'Tanks: User does not have any tanks!'
+
+
+  end
+
 
   describe "when users exists" do
 
@@ -121,7 +140,93 @@ describe "Users page" do
 
     end
 
+    it "user should not be able to visit edit user page and will be redirected correctly" do
+
+      visit edit_user_path(user)
+
+      expect(current_path).to eq(signin_path)
+
+
+      expect(page).to have_content 'You should be signed in'
+
+    end
+
+
+
   end
+
+  describe "when user is signed in" do
+
+    let!(:user){FactoryGirl.create(:user)}
+
+    before :each do
+      sign_in(name:"Testi", password:"Test1")
+
+    end
+
+    it "user should be able to visit edit user page and edit user correctly" do
+
+      visit edit_user_path(user)
+
+      expect(current_path).to eq(edit_user_path(user))
+      fill_in('user_password', with: "Muutos1")
+      fill_in('user_password_confirmation', with: "Muutos1")
+
+      click_button('Update User')
+
+      expect(current_path).to eq(user_path(user))
+
+      expect(page).to have_content 'User was successfully updated.'
+
+    end
+
+    it "user should be able to leave clan" do
+
+      visit user_path(user)
+
+      click_link('Leave clan')
+
+      expect(page).to have_content 'You have successfully left the clan!'
+      expect(page).to have_content 'Not a member of any clan!'
+
+    end
+
+    it "user should be able to join clan" do
+      FactoryGirl.create(:clan, name: "PARAS")
+      visit user_path(user)
+
+      click_link('Leave clan')
+      click_link('Join clan')
+
+      expect(current_path).to eq(join_clan_path)
+
+      select('PARAS', from:'user[clan_id]')
+
+      click_button('Join clan')
+
+      expect(page).to have_content "User's clan updated!"
+      expect(page).to have_content "Clan: PARAS"
+
+    end
+
+    it "user should be able to delete his account" do
+
+      visit users_path
+
+
+      click_link('Destroy')
+
+      expect(current_path).to eq(users_path)
+
+      expect(page).not_to have_content 'Testi'
+      expect(page).to have_content 'Number of users: 0'
+
+    end
+
+
+
+  end
+
 
 
 end
